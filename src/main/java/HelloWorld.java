70f923527373f58c471ebe8ba275be8e1c625e80
Dmitry Falin <falin.d.14@mail.ru>
2020-09-18 14:13:01 +0300

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorld extends HttpServlet {
+
+    private String message;
+
+    @Override
+    public void init() throws ServletException {
+        message = "Hello World";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>" + message + "</h1>");
+    }
+}
